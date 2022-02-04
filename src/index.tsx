42627c0733464dadf5cd1c00f2a6ba2ccc350e96
Rasskris <rassoshenko.kristina@yandex.ru>
2022-02-04 16:59:57 +0300

    init: start test task

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './styles/index.scss';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
+
+reportWebVitals();
